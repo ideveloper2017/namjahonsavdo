913512205039a895fcdca281b48272fbbfa61314
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-13 22:34:23 +0500

    commit

--- platform/themes/namjahon/views/page.blade.php
+++ platform/themes/namjahon/views/page.blade.php
@@ -13,11 +13,11 @@
     @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($page)))
         {!! render_object_gallery($galleries) !!}
     @endif
-    {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!}
+    {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, $page->content, $page) !!}
 
 @else
     @if (defined('GALLERY_MODULE_SCREEN_NAME') && !empty($galleries = gallery_meta_data($page)))
         {!! render_object_gallery($galleries) !!}
     @endif
-    {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content), $page) !!}
+    {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, $page->content, $page) !!}
 @endif
