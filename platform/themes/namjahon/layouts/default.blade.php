71991c7fd8cf62a8881c883d98daf7083faff6d8
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-11-14 21:46:24 +0500

    commit

--- platform/themes/namjahon/layouts/default.blade.php
+++ platform/themes/namjahon/layouts/default.blade.php
@@ -3,7 +3,11 @@
 {!! Theme::partial('breadcrumbs') !!}
 
 <main class="page-content">
+    <div class="tm-section blogs-area bg-white tm-padding-section">
+        <div class="container">
     {!! Theme::content() !!}
+        </div>
+    </div>
 </main>
 
 {!! Theme::partial('footer') !!}
